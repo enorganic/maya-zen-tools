7aea32bd96ff05ec5403a5cfe2ca8c11b01e56e4
David Belais <david@belais.me>
2025-06-19 13:07:54 -0700

    Create Autodesk App Store Add-in Package

---
+++ ApplicationAddins/ZenTools/Contents/scripts/ZenTools_load.py
@@ -0,0 +1,21 @@
+"""
+This script will check to see if the `maya-zen-tools` python package
+is installed, and if not, install it—otherwise, it will load the add-in.
+"""
+
+import os
+from pathlib import Path
+from subprocess import check_call
+
+try:
+    from maya_zen_tools import startup
+except ImportError:
+    print("Installing ZenTools for Maya")
+    mayapy: str = str(
+        Path(os.environ.get("MAYA_LOCATION")).joinpath("bin", "mayapy")
+    )
+    check_call([mayapy, "-m", "pip", "install", "maya-zen-tools"])
+    from maya_zen_tools import install, startup
+
+    install.main()
+    startup.main()
